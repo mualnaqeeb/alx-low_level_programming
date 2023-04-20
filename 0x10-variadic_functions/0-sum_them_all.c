1058477ada487319d8317f2f6b15c83bc26bfefd
mualnaqeeb <alnaqeeb128@gmail.com>
2023-04-20 03:03:53 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,20 +1,22 @@
 #include <stdarg.h>
+
 /**
- * sum_them_all - sums all arguments
- * @n: the number of arguments
- * Return: the result of the args
+ * sum_them_all - sums all arguments passed
+ *
+ * @n: number of arguments
+ *
+ * Return: sum of arguments
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
-	unsigned int count;
-	int result = 0;
+	unsigned int i;
+	int sum;
+	va_list list;
 
-	if (n == 0)
-		return (0);
-	va_start(args, n);
-	for (count = 0; count < n; count++)
-		result += va_arg(args, int);
-	va_end(args);
-	return (result);
+	va_start(list, n);
+	for (i = 0, sum = 0; i < n; i++)
+		sum += va_arg(list, int);
+
+	va_end(list);
+	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,16 +1,25 @@
-#ifndef _VARIADIC_FUNCTIONS_H_
-#define _VARIADIC_FUNCTIONS_H_
+#ifndef VARIADICFUNCTIONSH
+#define VARIADICFUNCTIONSH
 #include <stdarg.h>
 
-typedef struct types
-{
-	char z;
-	void (*f)(va_list);
-} types_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 int _putchar(char c);
 
-#endif /* variadic_functions.h */
+/**
+ * struct funckey - a key for picking functions to use for printing
+ * in 3-print_all.c
+ *
+ * @f: function to use for printing
+ * @spec: character specifier
+ */
+typedef struct funckey
+{
+	void (*f)(va_list);
+	char spec;
+} funckey;
+
+#endif
