e7c59afcc6069387bfc59818bf4a9709d7e8349a
mualnaqeeb <alnaqeeb128@gmail.com>
2023-04-20 03:02:29 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,27 +1,20 @@
-#include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
 /**
- * sum_them_all - sum of all its parameters
- * @n: number of arguements
- * Return: sum of all parameters or 0
- **/
+ * sum_them_all - sums all arguments
+ * @n: the number of arguments
+ * Return: the result of the args
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list all_sum;
-	int total = 0;
-	unsigned int i;
+	va_list args;
+	unsigned int count;
+	int result = 0;
 
 	if (n == 0)
 		return (0);
-
-	va_start(all_sum, n);
-
-	for (i = 0; i < n; i++)
-		total = total + va_arg(all_sum, unsigned int);
-
-	va_end(all_sum);
-	return (total);
-
+	va_start(args, n);
+	for (count = 0; count < n; count++)
+		result += va_arg(args, int);
+	va_end(args);
+	return (result);
 }
-
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,22 +1,16 @@
-#ifndef variadic_functions_h
-#define variadic_functions_h
-
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-/**
- * struct types_t - Struct types
- *
- * @types: The operator
- * @f: The function associated
- */
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+#include <stdarg.h>
 
 typedef struct types
 {
 	char z;
 	void (*f)(va_list);
 } types_t;
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
 
-#endif
-
+#endif /* variadic_functions.h */
