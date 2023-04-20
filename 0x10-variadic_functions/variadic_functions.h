450a230c2486e3b89e9ca59c55632747ee8ec1a9
mualnaqeeb <alnaqeeb128@gmail.com>
2023-04-20 02:54:58 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,6 +11,5 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int _putchar(char c);
 
-#endif /* variadic_functions.h */
+#endif 
