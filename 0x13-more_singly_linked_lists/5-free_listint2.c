fe08e47c4e70c5217fd30671005f3d25f992c92d
mualnaqeeb <alnaqeeb128@gmail.com>
2023-04-29 22:21:41 +0300

    add fix style

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,10 +1,9 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a listint_t list and sets head to NULL
- * @head: double pointer to the list
+ * free_listint2 - Frees a list
+ * @head: Pointer to address of a list
  */
-
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
