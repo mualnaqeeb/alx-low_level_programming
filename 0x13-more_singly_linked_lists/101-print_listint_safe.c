c1a57fe4c278e55ea72bcb642a8f5abe3f762331
mualnaqeeb <alnaqeeb128@gmail.com>
2023-04-29 22:27:49 +0300

    update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,21 +6,33 @@
  *
  * Return: Number of nodes in list
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t c = 0;
+    const listint_t *current, *next;
+    size_t count = 0;
 
-	while (head && head > head->next)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		++c;
-	}
-	if (head)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-		++c;
-	}
-	return (c);
+    if (head == NULL)
+        exit(98);
+
+    current = head;
+
+    while (current != NULL)
+    {
+        printf("[%p] %d\n", (void *)current, current->n);
+        count++;
+
+        next = current->next;
+
+        /* Check if the next node has already been visited */
+        if (next != NULL && next <= current)
+        {
+            printf("-> [%p] %d\n", (void *)next, next->n);
+            exit(98);
+        }
+
+        current = next;
+    }
+
+    return (count);
 }
