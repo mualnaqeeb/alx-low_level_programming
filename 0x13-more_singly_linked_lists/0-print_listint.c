862c4160978780ef8d6c77ca582757326032ce93
mualnaqeeb <alnaqeeb128@gmail.com>
2023-04-29 05:10:14 -0700

    fix4

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_listint - prints all the elements of a listint_t list
@@ -14,7 +15,7 @@ while (h)
 {
 printf("%d\n", h->n);
 h = h->next;
-count++;
+++count;
 }
 
 return (count);
