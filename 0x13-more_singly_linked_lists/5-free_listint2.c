c837ea2d31e40ac96a74e47e8a29f78b3f3fa243
mualnaqeeb <alnaqeeb128@gmail.com>
2023-04-29 22:23:02 +0300

    add new line

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * free_listint2 - Frees a list
- * @head: Pointer to address of a list
+ * free_listint2 - frees a listint_t list and sets head to NULL
+ * @head: double pointer to the list
  */
+
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
