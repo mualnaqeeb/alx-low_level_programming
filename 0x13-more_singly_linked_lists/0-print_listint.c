e4143c4c54dd23c200654d466a0b73d8327c53fe
mualnaqeeb <alnaqeeb128@gmail.com>
2023-04-29 04:56:17 -0700

    fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,17 +7,17 @@
  *
  * Return: the number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+size_t count = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
-	}
-
-	return (count);
+return (count);
 }
 
